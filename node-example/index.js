473cde1a37fd9afb4c1be78a2d9fccec9b0bd3b5
Nico <nicholas.computacao@outlook.com>
2022-09-06 18:55:25 -0300

    feat: added docker-compose implementation and mongodb

--- node-example/index.js
+++ node-example/index.js
@@ -1,4 +1,18 @@
 const express = require('express');
 const app = express();
-app.use((_req, res) => res.send('Hello world'));
+
+const uri = "mongodb://mongo:27017/test-docker";
+
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(async (_req, res) => {
+    await client.connect();
+    const db = client.db();
+    const testsCollection = db.collection('tests');
+    const tests = await testsCollection.find().toArray();
+    await testsCollection.insertOne({ name: Math.random().toString(36).substring(7) });
+    res.send(JSON.stringify(tests));
+});
+
 app.listen(3000, () => console.log('Listening on port 3001'));
